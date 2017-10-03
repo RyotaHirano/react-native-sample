d21438c146794436848ba5cc38d95780a4fdd5a9
ryotahirano <hirano@tsumikiinc.com>
2017-10-03 12:27:55 +0900

    jest add PageA

--- App.test.js
+++ App.test.js
@@ -1,5 +1,8 @@
+import 'react-native';
+import { StackNavigator } from 'react-navigation'
 import React from 'react';
 import App from './App';
+import PageAScreen from './components/PageAScreen'
 
 import renderer from 'react-test-renderer';
 
@@ -7,3 +10,19 @@ it('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered).toBeTruthy();
 });
+
+test('render PageA', () => {
+  const App = StackNavigator({
+    PageA: {
+      screen: PageAScreen
+    },
+  })
+  const navigation = App
+
+  let tree = renderer.create(
+    <PageAScreen navigation={navigation}/>
+  );
+
+  tree = tree.toJSON()
+  expect(tree).toMatchSnapshot();
+})
